74ceecaf6232a7bd3427f81ba4c2dac6a65400e0
luigiberducci <luigi.berd@gmail.com>
2023-09-19 20:44:14 +0200

    simulate multiple examples

--- examples/example.py
+++ examples/example.py
@@ -1,17 +1,26 @@
+from time import sleep
+
+import numpy as np
+
 from envs.control_wrapper import AutoControlWrapper
 from envs.multi_agent_env import SimpleEnv
 
 env = SimpleEnv(render_mode="human")
 env = AutoControlWrapper(env)
 
-env.reset(seed=42)
-env.render()
+seed = 42
+n_episodes = 10
 
-print(env.observation_space)
-
-done, truncated = False, False
-while not done and not truncated:
-    actions = env.action_space.sample()
-    obs, reward, done, truncated, info = env.step(actions)
+for i in range(n_episodes):
+    env.reset(seed=seed + i)
     env.render()
-    print("reward: {}, done: {}".format(reward, done))
+
+    print(env.observation_space)
+
+    done, truncated = False, False
+    while not done and not truncated:
+        actions = env.action_space.sample()
+        obs, reward, done, truncated, info = env.step(actions)
+        env.render()
+        sleep(0.1)
+        print("reward: {}, done: {}".format(reward, done))
