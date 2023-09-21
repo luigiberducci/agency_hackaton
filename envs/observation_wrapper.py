136e8448151eab1f09931c12d9480453c694054c
harryjkp <harryjkp@gmail.com>
2023-09-21 22:51:43 +0100

    Port existing wrapper

--- envs/observation_wrapper.py
+++ envs/observation_wrapper.py
@@ -3,9 +3,21 @@ import gymnasium as gym
 
 class RGBImgObsWrapper(gym.core.ObservationWrapper):
     """
-    Wrapper to use fully observable RGB image as the only observation output,
-    no language/mission. This can be used to have the agent to solve the
-    gridworld in pixel space.
+    Wrapper to use fully observable RGB image as observation,
+    This can be used to have the agent to solve the gridworld in pixel space.
+
+    Example:
+        >>> import gymnasium as gym
+        >>> import matplotlib.pyplot as plt
+        >>> from minigrid.wrappers import RGBImgObsWrapper
+        >>> env = gym.make("MiniGrid-Empty-5x5-v0")
+        >>> obs, _ = env.reset()
+        >>> plt.imshow(obs['image'])  # doctest: +SKIP
+        ![NoWrapper](../figures/lavacrossing_NoWrapper.png)
+        >>> env = RGBImgObsWrapper(env)
+        >>> obs, _ = env.reset()
+        >>> plt.imshow(obs['image'])  # doctest: +SKIP
+        ![RGBImgObsWrapper](../figures/lavacrossing_RGBImgObsWrapper.png)
     """
 
     def __init__(self, env, tile_size=8):
@@ -13,23 +25,24 @@ class RGBImgObsWrapper(gym.core.ObservationWrapper):
 
         self.tile_size = tile_size
 
-        self.observation_space.spaces['image'] = spaces.Box(
+        new_image_space = spaces.Box(
             low=0,
             high=255,
-            shape=(self.env.width*tile_size, self.env.height*tile_size, 3),
-            dtype='uint8'
+            shape=(
+                self.unwrapped.width * tile_size,
+                self.unwrapped.height * tile_size,
+                3,
+            ),
+            dtype="uint8",
+        )
+
+        self.observation_space = spaces.Dict(
+            {**self.observation_space.spaces, "image": new_image_space}
         )
 
     def observation(self, obs):
-        env = self.unwrapped
-
-        rgb_img = env.render(
-            render_mode='rgb_array',
-            # highlight=False,
-            # tile_size=self.tile_size
+        rgb_img = self.get_frame(
+            highlight=self.unwrapped.highlight, tile_size=self.tile_size
         )
 
-        return {
-            # 'mission': obs['mission'],
-            'image': rgb_img
-        }
+        return {**obs, "image": rgb_img}
