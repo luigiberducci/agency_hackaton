b58a6c218eec5080c5ea886d66abc39c35195a57
harryjkp <harryjkp@gmail.com>
2023-09-23 00:09:23 +0100

    Add flag to hide goal

--- envs/observation_wrapper.py
+++ envs/observation_wrapper.py
@@ -20,10 +20,11 @@ class RGBImgObsWrapper(gym.core.ObservationWrapper):
         ![RGBImgObsWrapper](../figures/lavacrossing_RGBImgObsWrapper.png)
     """
 
-    def __init__(self, env, tile_size=8):
+    def __init__(self, env, tile_size=8,hide_obj_types: list[str] = []):
         super().__init__(env)
 
         self.tile_size = tile_size
+        self.hide_obj_types = hide_obj_types
 
         new_image_space = spaces.Box(
             low=0,
@@ -42,7 +43,8 @@ class RGBImgObsWrapper(gym.core.ObservationWrapper):
 
     def observation(self, obs):
         rgb_img = self.get_frame(
-            highlight=self.unwrapped.highlight, tile_size=self.tile_size
+            highlight=self.unwrapped.highlight, tile_size=self.tile_size,
+            hide_obj_types=self.hide_obj_types
         )
 
         return {**obs, "image": rgb_img}
