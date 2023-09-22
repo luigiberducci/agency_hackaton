114d1f6f07f4ee54cf1036afee029f85ab10cba6
luigiberducci <luigi.berd@gmail.com>
2023-09-22 13:00:03 +0200

    update examples, remove pyyaml requirement, update gym-multigrid version

--- examples/example.py
+++ examples/example.py
@@ -1,22 +1,32 @@
+import logging
+
 import gymnasium as gym
 
 import envs  # keep it, otherwise gym.make() won't work on custom envs
-from envs.control_wrapper import AutoControlWrapper
+from envs.control_wrapper import AutoControlWrapper, UnwrapSingleAgentDictWrapper
+from envs.manual_control import ManualControl
 from envs.observation_wrapper import RGBImgObsWrapper
 
 
 env = gym.make("one-door-2-agents-v0", render_mode="human", render_fps=250)
 env = AutoControlWrapper(env)
 env = RGBImgObsWrapper(env)
+# env = UnwrapSingleAgentDictWrapper(env)
+# env = ManualControl(env)
+# env.start()
+
+print(env.action_space)
+print(env.observation_space)
 
 seed = 42
 n_episodes = 3
+
 
 for i in range(n_episodes):
     env.reset(seed=seed + i)
     env.render()
 
-    print(env.observation_space)
+    print(f"Episode {i}")
 
     done, truncated = False, False
     while not done and not truncated:
@@ -24,3 +34,6 @@ for i in range(n_episodes):
         obs, reward, done, truncated, info = env.step(actions)
         env.render()
         print("reward: {}, done: {}".format(reward, done))
+    print()
+
+env.close()
--- examples/example_no_render.py
+++ examples/example_no_render.py
@@ -10,14 +10,18 @@ env = gym.make("one-door-2-agents-v0")
 env = AutoControlWrapper(env)
 env = RGBImgObsWrapper(env)
 
+print(env.action_space)
+print(env.observation_space)
+
 seed = 42
 n_episodes = 3
+
 
 for i in range(n_episodes):
     env.reset(seed=seed + i)
     env.render()
 
-    print(env.observation_space)
+    print(f"Episode {i}")
 
     done, truncated = False, False
     while not done and not truncated:
@@ -25,3 +29,6 @@ for i in range(n_episodes):
         obs, reward, done, truncated, info = env.step(actions)
         env.render()
         print("reward: {}, done: {}".format(reward, done))
+    print()
+
+env.close()
