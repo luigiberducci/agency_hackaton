6d9e4a60759be95aa463a31c8c979cbb7cd4317a
luigiberducci <luigi.berd@gmail.com>
2023-09-23 23:06:42 +0200

    add example, update gym-multigrid commit, linting

--- envs/goal_changing_wrapper.py
+++ envs/goal_changing_wrapper.py
@@ -7,21 +7,25 @@ from gym_multigrid.world_objects import Goal
 
 
 class GoalChangingWrapper(gymnasium.Wrapper):
-    def __init__(self, env: Env, goal_generator: str = None,p_change: float = 0.1, **kwargs):
+    def __init__(
+        self, env: Env, goal_generator: str = None, p_change: float = 0.1, **kwargs
+    ):
         if goal_generator is not None:
             self.goal_generator = goal_generator_factory(goal_generator, **kwargs)
         else:
             self.goal_generator = None
         self.p_change = p_change
         super().__init__(env)
-    
-    def step(self, action: Any) -> tuple[Any, SupportsFloat, bool, bool, dict[str, Any]]:
-        
+
+    def step(
+        self, action: Any
+    ) -> tuple[Any, SupportsFloat, bool, bool, dict[str, Any]]:
         if np.random.random() < self.p_change:
             self.env.goals = []
-            self.env.grid.grid = [obj if not isinstance(obj, Goal) else None for obj in self.env.grid.grid ]
+            self.env.grid.grid = [
+                obj if not isinstance(obj, Goal) else None for obj in self.env.grid.grid
+            ]
             for i in range(self.num_agents):
-
                 if i == 0:
                     goal_pos = None
                 elif self.goal_generator is not None:
