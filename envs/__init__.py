7ca7b911e9bcca4f233649ec3749a40e9229ecbf
harryjkp <harryjkp@gmail.com>
2023-09-24 09:14:27 +0100

    Allow goal to spawn in the corridor

--- envs/__init__.py
+++ envs/__init__.py
@@ -18,6 +18,15 @@ gym.register(
     },
 )
 
+goal_top_bottom_rows = [(x, y) for x in range(5, 11) for y in [1, 4]]
+
+gym.register(
+    "one-door-2-agents-goal-change-v0",
+    entry_point="envs.door_env:DoorEnv",
+    kwargs={"width": 12, "height": 6, "num_agents": 2, "max_steps": 1000,
+            "goal_generator": "choice", "goals": goal_top_bottom_rows},
+)
+
 gym.register(
     "one-door-3-agents-v0",
     entry_point="envs.door_env:DoorEnv",
@@ -34,6 +43,14 @@ gym.register(
             "goal_generator": "choice", "goals": goal_top_bottom_rows},
 )
 
+goal_top_bottom_rows = [(x, y) for x in range(5, 11) for y in [1, 4]]
+
+gym.register(
+    "two-doors-2-agents-goal-change-v0",
+    entry_point="envs.two_doors_env:TwoDoorsEnv",
+    kwargs={"width": 12, "height": 6, "num_agents": 2, "max_steps": 1000,
+            "goal_generator": "choice", "goals": goal_top_bottom_rows},
+)
 
 goal_top_skewed_distr = [1.0 if y == 4 else 5.0 for x in range(5, 9) for y in [1, 4]]
 goal_bottom_skewed_distr = [1.0 if y == 1 else 5.0 for x in range(5, 9) for y in [1, 4]]
